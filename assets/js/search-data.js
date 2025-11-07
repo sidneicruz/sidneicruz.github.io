e4fc3653c1d8d369634061ecb4cbe2a0960d7f79
sidneicruz <sidnei.cruz@gmail.com>
2025-11-07 19:07:38 +0000

    Deploying to gh-pages from @ sidneicruz/sidneicruz.github.io@5365717fdf72dd07d33eee190a8f3e266d04cc19 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,35 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("mailto:%73%69%64%6E%65%69.%63%72%75%7A@%74%65%63%6E%69%63%6F.%75%6C%69%73%62%6F%61.%70%74", "_blank");
         },
       },{
       id: 'light-theme',
